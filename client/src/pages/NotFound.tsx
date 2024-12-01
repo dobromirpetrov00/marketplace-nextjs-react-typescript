6fb49e343e22b47fa9c607e0c3bf460522260a7f
Dobby <godelbit@gmail.com>
2024-12-01 21:59:42 +0200

    add not found page

---
+++ client/src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+/**
+ * Renders a "Not Found" page component.
+ * This component is typically used to display a 404 error page when a requested resource is not found.
+ */
+const NotFound = () => {
+  return ( <div>Not Found</div> );
+}
+
+export default NotFound;
